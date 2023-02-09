f1c96c1f773c149cfb4fd206d46d010ced5f8e31
palmuves <vesa.palmu@tietoevry.com>
2023-02-09 11:37:13 +0200

    separated index and app files

--- index.js
+++ index.js
@@ -1,33 +1,7 @@
-const express = require('express');
-const app = express();
-const cors = require('cors');
-const config = require('./utils/config');
+const app = require('./app');
+const { PORT } = require('./utils/config');
 const logger = require('./utils/logger');
-const Blog = require('./model/blog');
 
-app.use(cors());
-app.use(express.json());
-
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs);
-    });
-});
-
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body);
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result);
-    });
-});
-
-const PORT = config.PORT;
 app.listen(PORT, () => {
   logger.info(`Server running on port ${PORT}`);
 });
