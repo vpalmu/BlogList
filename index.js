7bdd073468c9638a7cb855f5fd0a6ea72ac4fcd9
palmuves <vesa.palmu@tietoevry.com>
2023-02-09 11:31:56 +0200

    added missing semicolons

--- index.js
+++ index.js
@@ -1,55 +1,33 @@
-const http = require('http')
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const mongoose = require('mongoose')
-const config = require('./utils/config')
-const logger = require('./utils/logger')
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const config = require('./utils/config');
+const logger = require('./utils/logger');
+const Blog = require('./model/blog');
 
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
-
-const Blog = mongoose.model('Blog', blogSchema)
-
-const mongoUrl = config.MONGODB_URI
-mongoose.set('strictQuery', false)
-logger.info('connecting to', config.MONGODB_URI)
-
-mongoose.connect(mongoUrl)
-  .then(_ => {
-    logger.info('connected to MongoDB')
-  })
-  .catch((error) => {
-    logger.info('error connecting to MongoDB:', error.message)
-  })
-
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
 app.get('/api/blogs', (request, response) => {
   Blog
     .find({})
     .then(blogs => {
-      response.json(blogs)
-    })
-})
+      response.json(blogs);
+    });
+});
 
 
 app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
+  const blog = new Blog(request.body);
 
   blog
     .save()
     .then(result => {
-      response.status(201).json(result)
-    })
-})
+      response.status(201).json(result);
+    });
+});
 
-const PORT = config.PORT
+const PORT = config.PORT;
 app.listen(PORT, () => {
-  logger.info(`Server running on port ${PORT}`)
-})
+  logger.info(`Server running on port ${PORT}`);
+});
--- utils/config.js
+++ utils/config.js
@@ -1,20 +1,20 @@
-const logger = require('./logger')
+const logger = require('./logger');
 
 // use PORT from .env file only when running locally. In production, Fly.io defines the PORT
 // dynamically via it's own environment variable
 //
 if (process.env.NODE_ENV !== 'production') {
-  logger.info('development environment..')
+  logger.info('development environment..');
   // With dotenv usage, the environment variables defined in the .env file override the values in defined in environment (OS)
-  require('dotenv').config()
-  logger.info('process.env.PORT:', process.env.PORT)
-  logger.info('process.env.MONGODB_URI:', process.env.MONGODB_URI)
+  require('dotenv').config();
+  logger.info('process.env.PORT:', process.env.PORT);
+  logger.info('process.env.MONGODB_URI:', process.env.MONGODB_URI);
 }
 
-const PORT = process.env.PORT || 3001
-const MONGODB_URI = process.env.MONGODB_URI
+const PORT = process.env.PORT || 3001;
+const MONGODB_URI = process.env.MONGODB_URI;
 
 module.exports = {
   MONGODB_URI,
   PORT
-}
+};
