409b5eb2525f91c6dc5e01e94a9ed7659726bd9f
palmuves <vesa.palmu@tietoevry.com>
2023-02-13 13:37:45 +0200

    initializing db before each test

--- tests/blogs_api.test.js
+++ tests/blogs_api.test.js
@@ -1,8 +1,24 @@
 const mongoose = require('mongoose');
 const supertest = require('supertest');
 const app = require('../app');
+const Blog = require('../model/blog');
+const logger = require('../utils/logger');
+const listHelper = require('../test_utils/list_helper');
 
 const api = supertest(app);
+
+beforeEach(async () => {
+  await Blog.deleteMany({});
+  logger.info('db cleared');
+
+  for (let blog of listHelper.listWithManyBlogs) {
+    let blogObject = new Blog(blog);
+    await blogObject.save();
+    logger.info('initial blog saved to db');
+  }
+
+  logger.info('db done');
+});
 
 describe('blog list tests', () => {
   test('blogs are returned as json', async () => {
@@ -12,11 +28,10 @@ describe('blog list tests', () => {
       .expect('Content-Type', /application\/json/);
 
     const contents = response.body.map(r => r.content);
-    expect(contents).toHaveLength(3);
+    expect(contents).toHaveLength(6);
 
   }, 100000);
 });
-
 afterAll(async () => {
   await mongoose.connection.close();
 });
--- tests/list_helper.test.js
+++ tests/list_helper.test.js
@@ -1,56 +1,5 @@
 const listHelper = require('../test_utils/list_helper');
 
-const listWithManyBlogs = [
-  {
-    _id: '5a422a851b54a676234d17f7',
-    title: 'React patterns',
-    author: 'Michael Chan',
-    url: 'https://reactpatterns.com/',
-    likes: 7,
-    __v: 0
-  },
-  {
-    _id: '5a422aa71b54a676234d17f8',
-    title: 'Go To Statement Considered Harmful',
-    author: 'Edsger W. Dijkstra',
-    url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
-    likes: 5,
-    __v: 0
-  },
-  {
-    _id: '5a422b3a1b54a676234d17f9',
-    title: 'Canonical string reduction',
-    author: 'Edsger W. Dijkstra',
-    url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html',
-    likes: 12,
-    __v: 0
-  },
-  {
-    _id: '5a422b891b54a676234d17fa',
-    title: 'First class tests',
-    author: 'Robert C. Martin',
-    url: 'http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll',
-    likes: 10,
-    __v: 0
-  },
-  {
-    _id: '5a422ba71b54a676234d17fb',
-    title: 'TDD harms architecture',
-    author: 'Robert C. Martin',
-    url: 'http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html',
-    likes: 0,
-    __v: 0
-  },
-  {
-    _id: '5a422bc61b54a676234d17fc',
-    title: 'Type wars',
-    author: 'Robert C. Martin',
-    url: 'http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html',
-    likes: 2,
-    __v: 0
-  }
-];
-
 describe('dummy tests', () => {
   const blogs = [];
 
@@ -83,14 +32,14 @@ describe('total likes', () => {
   });
 
   test('of bigger list is calculated right', () => {
-    const result = listHelper.totalLikes(listWithManyBlogs);
+    const result = listHelper.totalLikes(listHelper.listWithManyBlogs);
     expect(result).toBe(36);
   });
 });
 
 describe('favorite blog', () => {
   test('of list is found', () => {
-    const result = listHelper.favoriteBlog(listWithManyBlogs);
+    const result = listHelper.favoriteBlog(listHelper.listWithManyBlogs);
 
     expect(result.author).toBe('Edsger W. Dijkstra');
     expect(result.title).toBe('Canonical string reduction');
@@ -100,7 +49,7 @@ describe('favorite blog', () => {
 
 describe('most blogs', () => {
   test('author with most blogs is found', () => {
-    const result = listHelper.mostBlogs(listWithManyBlogs);
+    const result = listHelper.mostBlogs(listHelper.listWithManyBlogs);
 
     expect(result.author).toBe('Robert C. Martin');
     expect(result.blogQty).toBe(3);
@@ -109,7 +58,7 @@ describe('most blogs', () => {
 
 describe('most liked', () => {
   test('author with most likes is found', () => {
-    const result = listHelper.mostLikes(listWithManyBlogs);
+    const result = listHelper.mostLikes(listHelper.listWithManyBlogs);
 
     expect(result.author).toBe('Edsger W. Dijkstra');
     expect(result.likes).toBe(17);
