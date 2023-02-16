e766cf2ff61e0a2902033cc3d6f5621a0fd55a06
palmuves <vesa.palmu@tietoevry.com>
2023-02-16 16:06:07 +0200

    added login and jwt token generation

---
+++ controllers/login.controller.js
@@ -0,0 +1,34 @@
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
+const User = require('../model/user');
+
+// eslint-disable-next-line no-unused-vars
+const connect = require('../model/connectDb'); // 'connect' is used just to open connection with db
+
+async function loginBlog(request, response) {
+  const { username, password } = request.body;
+
+  const user = await User.findOne({ username });
+  const passwordCorrect = user === null
+    ? false
+    : await bcrypt.compare(password, user.passwordHash);
+
+  if (!(user && passwordCorrect)) {
+    return response.status(401).json({
+      error: 'invalid username or password'
+    });
+  }
+
+  const userForToken = {
+    username: user.username,
+    id: user._id,
+  };
+
+  const token = jwt.sign(userForToken, process.env.SECRET);
+
+  response
+    .status(200)
+    .send({ token, username: user.username, name: user.name });
+}
+
+module.exports = { loginBlog };
--- routes/bloglist.routes.js
+++ routes/bloglist.routes.js
@@ -2,6 +2,10 @@ const express = require('express');
 const router = express.Router();
 const blogController = require('../controllers/blog.controller');
 const userController = require('../controllers/user.controller');
+const loginController = require('../controllers/login.controller');
+
+// login
+router.post('/api/login', loginController.loginBlog);
 
 // blogs
 router.get('/api/blogs', blogController.getAll);
