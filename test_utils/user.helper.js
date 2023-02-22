ad06b3717590473c8476d600414f27e75c745a46
palmuves <vesa.palmu@tietoevry.com>
2023-02-22 11:50:27 +0200

    added some methods for user handling and token generation

--- test_utils/user.helper.js
+++ test_utils/user.helper.js
@@ -1,8 +1,17 @@
 const User = require('../model/user');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 const usersInDb = async () => {
   const users = await User.find({});
   return users.map(u => u.toJSON());
+};
+
+const createRootUser = async () => {
+  const passwordHash = await bcrypt.hash('sekret', 10);
+  const user = new User({ username: 'root', name: 'Root Man', passwordHash });
+
+  await user.save();
 };
 
 const getRootUser = async () => {
@@ -10,7 +19,20 @@ const getRootUser = async () => {
   return root;
 };
 
+const getTokenForRootUser = async () => {
+  const rootUser = await getRootUser();
+  const userForToken = {
+    username: rootUser.username,
+    id: rootUser._id,
+  };
+
+  const token = 'Bearer ' + jwt.sign(userForToken, process.env.SECRET);
+  return token;
+};
+
 module.exports = {
   usersInDb,
-  getRootUser
+  createRootUser,
+  getRootUser,
+  getTokenForRootUser
 };
